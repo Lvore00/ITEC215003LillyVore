d5b3152f9c43f0de5eb5162ca3015d85bd95d2d9
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-08-23 15:27:24 -0400

    Update PrimativeParameters.java

--- PrimativeParameters.java
+++ PrimativeParameters.java
@@ -3,13 +3,15 @@ public class PrimitiveParameters {
         int a = 10, b = 5;
         System.out.println("Before incorrectSwap: a=%d, b=%d\n", a, b);
         incorrectSwap(a, b);
-        System.out.println("After incorrectSwap: a=%d, b=%d\n", a, b);  // Corrections: d and n are not mentioned in the code, printf is changed to println
+        System.out.println("After incorrectSwap: a=%d, b=%d\n", a, b); 
+        System.out.println("After incorrectSwap: a=%d, b=%d\n", tmp, tmp2); 
     }
 
     public static void incorrectSwap(int a, int b) {
         int tmp = a;
-        a = b;
         b = tmp;
+        int tmp2 = b;
+        a = tmp2;
         
     }
 }
