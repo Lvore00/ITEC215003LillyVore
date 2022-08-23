884109312e5522dbff997419c1ee6bb26e01615a
Lilly <111086892+Lvore00@users.noreply.github.com>
2022-08-23 17:08:42 -0400

    Create Roll.java

---
+++ Roll.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+      
+      
+      public static int roll(int min, int max) {
+        int range = max - min;
+        return (int)((Math.random() * max + min) + min; // adding 1 or min to max 
+    }
+      
+      
+      
+    }
